7da20be9b5f951606148ec528e16ee1dc0f68eba
shyren <121025871+Shyren12@users.noreply.github.com>
2025-02-21 15:51:25 +0700

    topic 6 7

---
+++ apiMongoDB/config/index.ts
@@ -0,0 +1,3 @@
+export const MONGO_URI = "mongodb+srv://viptuan12062003:12062003shyren@testconnectcluster.plflm.mongodb.net/?retryWrites=true&w=majority&appName=testConnectCluster";
+
+export const PORT = process.env.PORT || 9000;
